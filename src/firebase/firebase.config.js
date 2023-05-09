686145f017fbb8eb006e7612ab3814659f7c8207
soubj <sobujahmed124@gmail.com>
2023-05-09 19:41:39 +0600

    new project for firebase auth

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8_fPur34wMCKFADohK8-mj0sGB4oWgY0",
+  authDomain: "hotel-page-firebase-practice.firebaseapp.com",
+  projectId: "hotel-page-firebase-practice",
+  storageBucket: "hotel-page-firebase-practice.appspot.com",
+  messagingSenderId: "999923677407",
+  appId: "1:999923677407:web:88ebb3b08e53a97e35916a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
