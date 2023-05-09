15bfa012b32771539d348c4eb71e2ef034f8e973
soubj <sobujahmed124@gmail.com>
2023-05-09 20:46:42 +0600

    completed context api setup

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,5 +1,17 @@
+import { createContext } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
 const Context = ({ children }) => {
-  return children;
+  const userInfo = {};
+
+  return (
+    <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default Context;
